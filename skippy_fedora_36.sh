71784d52782136a1c5412e819224df143c7dd3fc
Skippy <steelstone3@gmail.com>
2022-05-15 22:54:57 +0100

    lightweight cinnamon enviroment enviroment

--- skippy_fedora_36.sh
+++ skippy_fedora_36.sh
@@ -6,11 +6,26 @@ echo "fastestmirror=True" >> etc/dnf/dnf.conf
 echo "Updating system"
 sudo dnf -y update
 
-#echo "Installing log in manager"
-#sudo dnf -y install lightdm
+echo "Installing log in manager"
+sudo dnf -y install lightdm
+sudo systemctl enable lightdm
+sudo systemctl set-defaul graphical.target
 
 echo "Installing desktop enviroment"
-sudo dnf -y group install "Cinnamon Desktop"
+sudo dnf install cinnamon cinnamon-control-center cinnamon-screensaver
+# sudo dnf group install "Cinnamon Desktop"
+# sudo dnf group install "base-x"
+# sudo dnf group install "Cinnamon"
+# sudo dnf group install "Core"
+# sudo dnf group install "Dial-up Networking Support"
+# sudo dnf group install "Fonts"
+# sudo dnf group install "Guest Desktop Agents"
+# sudo dnf group install "Hardware Support"
+# sudo dnf group install "Input Methods"
+# sudo dnf group install "Multimedia"
+# sudo dnf group install "Common NetworkManager Submodules"
+# sudo dnf group install "Printing Support"
+# sudo dnf group install "Standard"
 
 echo "exec /usr/bin/cinnamon-session" >> ~/.xinitrc
 
