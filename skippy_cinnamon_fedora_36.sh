6aace0f4adaba58d218e048813f1771ee03dec0d
Alex Howson <alex@alex-pc.connect>
2022-05-16 01:27:33 +0100

    shuffled ordering of commands

---
+++ skippy_cinnamon_fedora_36.sh
@@ -0,0 +1,29 @@
+cd
+echo "Configuring DNF"
+echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+echo "fastestmirror=True" >> etc/dnf/dnf.conf
+
+echo "Updating system"
+sudo dnf update
+
+echo "Installing log in manager"
+sudo dnf install gdm
+sudo dnf remove gnome-desktop3 gnome-desktop4
+sudo systemctl enable gdm.service
+sudo systemctl set-defaul graphical.target
+
+echo "Installing desktop enviroment"
+sudo dnf install cinnamon
+sudo dnf group install "base-x"
+sudo dnf remove gnome-calendar
+
+echo "exec /usr/bin/cinnamon-session" >> ~/.xinitrc
+
+echo "Install applications"
+sudo dnf install git vim gnome-terminal gnome-calculator htop
+
+echo "Install browser"
+sudo dnf install dnf-plugins-core
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser
--- skippy_gnome_fedora_36.sh
+++ skippy_gnome_fedora_36.sh
@@ -13,17 +13,15 @@ sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install brave-browser
 
 echo "Install applications"
-sudo dnf install git vim gnome-terminal gnome-calculator htop
+sudo dnf install git vim gnome-terminal gnome-calculator htop nautilus geary
 
 echo "Installing log in manager"
 sudo dnf install gdm
-sudo dnf remove gnome-* gsound
 sudo systemctl enable gdm.service
 sudo systemctl set-defaul graphical.target
 
 echo "Installing desktop enviroment"
-sudo dnf install cinnamon
 sudo dnf install group "base-x"
 sudo dnf remove gnome-calendar
 
-echo "exec /usr/bin/cinnamon-session" >> ~/.xinitrc
+echo "exec /usr/bin/gnome-session" >> ~/.xinitrc
