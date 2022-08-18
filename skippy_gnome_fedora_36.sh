53f2f0a793cfea927dea9361b8f69fc704a4dc11
Skippy <70329398+Steelstone3@users.noreply.github.com>
2022-08-19 00:35:05 +0100

    Update skippy_gnome_fedora_36.sh

--- skippy_gnome_fedora_36.sh
+++ skippy_gnome_fedora_36.sh
@@ -1,27 +1,30 @@
 cd
 echo "Configuring DNF"
 sudo echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
-sudo echo "fastestmirror=True" >> etc/dnf/dnf.conf
+sudo echo "fastestmirror=True" >> /etc/dnf/dnf.conf
+sudo echo "deltarpm=1" >> /etc/dnf/dnf.conf
 
-echo "Updating system"
-sudo dnf update
-
-echo "Install browser"
+echo "Update system"
+sudo dnf upgrade --refresh
+sudo dnf autoremove
 sudo dnf install dnf-plugins-core
+sudo dnf install dnf-plugin-system-upgrade
+sudo dnf system-upgrade download --releasever=36
+
+echo "Install system sofware"
+sudo dnf install gdm dnf-plugins-core git vim gnome-terminal htop
+
+echo "Setup repositories"
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser
+
+echo "Install graphical enviroment"
+sudo dnf install @base-x gnome-shell
 
 echo "Install applications"
-sudo dnf install git vim gnome-terminal gnome-calculator htop nautilus geary
+sudo dnf install brave-browser gnome-calculator nautilus geary
 
-echo "Installing log in manager"
-sudo dnf install gdm
+echo "Enable services"
 sudo systemctl enable gdm.service
 sudo systemctl set-defaul graphical.target
-
-echo "Installing desktop enviroment"
-sudo dnf install group "base-x"
-sudo dnf remove gnome-calendar
-
 echo "exec /usr/bin/gnome-session" >> ~/.xinitrc
