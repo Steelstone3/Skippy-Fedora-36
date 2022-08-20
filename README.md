0b84b7d97554e4d614012154053a3ec4723e2506
Skippy <steelstone3@gmail.com>
2022-08-20 19:13:15 +0100

    Updated skippy fedora may come up with a version 2 later

--- README.md
+++ README.md
@@ -12,13 +12,87 @@ and the following sub-options
 > - Standard
 > - (if on a vitrtual machine) Guest Additions
 
-## Running The Script
+upon first boot install git and nano with the following command
+> sudo dnf install git nano
+
+### Optimising DNF
+
+Before starting the install it is a good idea to configure dnf (the package manager of Fedora) to speed up the process
+
+Edit the following file configuration with the following:
+
+> sudo dnf install nano
+> sudo nano /etc/dnf/dnf.conf
+
+Write the following at the bottom of the file
+
+> max_parallel_downloads=10
+> fastestmirror=True
+> deltarpm=1
+
+Save the file
+
+### Upgrading To Fedora 36 Server From An Older Version
+
+Please refer to this guide to upgrade to Fedora 36 (server) before going any further in this guide 
+> https://docs.fedoraproject.org/en-US/quick-docs/dnf-system-upgrade/#sect-performing-system-upgrade
+
+Typically run the following: 
+> sudo dnf upgrade --refresh
+> sudo dnf autoremove
+> sudo dnf install dnf-plugin-system-upgrade
+> sudo dnf system-upgrade download --releasever=36
+> sudo dnf system-upgrade reboot
+
+## Running The Script For Skippy Gnome Fedora 36
 Boot up Fedora Server and run the following commands
 
 > sudo dnf install git
 > git clone https://github.com/Steelstone3/Skippy-Fedora-36.git
 > cd Skippy-Fedora-36
-> bash skippy_fedora_36.sh
+> bash skippy_gnome_fedora_36.sh
 
-The script will then start to setup "Skippy Fedora"
+The script will then install the applications for "Skippy Gnome Fedora 36"
 
+## Configuration For Skippy Gnome Fedora 36
+Refer to this README.md for reference
+
+In order to get a working system the following files will need to be altered:
+
+### Setup xinitrc
+
+Create a file called .xinitrc in the home folder
+
+> touch ~/.xinitrc
+
+Then add the following line to run the graphical shell
+
+> exec /usr/bin/gnome-session ~/.xinitrc
+
+This will create a symlink to run the gnome shell on boot
+
+### Optimisations
+
+#### Wayland
+There are some optimisations that can be made.
+
+Wayland on Gnome works in cases of Intel or AMD graphics and can be enabled with the following:
+
+> sudo nano /etc/gdm/custom.conf
+
+Find this line "#WaylandEnable=false" under [daemon] within the file and change it to:
+
+> WaylandEnable=true
+
+Save the file
+
+#### Mitigations Performance Boost
+
+Follow this guide with caution https://christitus.com/speedup-linux/
+
+Within /etc/default/grub change GRUB_CMDLINE_LINUX="rhgb quiet to:
+
+> sudo nano /etc/default/grub
+> GRUB_CMDLINE_LINUX="rhgb quiet mitigations=off"
+
+Save the file
--- skippy_gnome_fedora_36.sh
+++ skippy_gnome_fedora_36.sh
@@ -1,38 +1,27 @@
-cd
-echo "Configuring DNF"
-sudo echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
-sudo echo "fastestmirror=True" >> /etc/dnf/dnf.conf
-sudo echo "deltarpm=1" >> /etc/dnf/dnf.conf
-
-echo "Update system"
-sudo dnf upgrade --refresh
-sudo dnf autoremove
-sudo dnf install dnf-plugins-core dnf-plugin-system-upgrade
-sudo dnf system-upgrade download --releasever=36
-
 echo "Install system sofware"
 sudo dnf install gdm git vim htop pipewire
 
 echo "Setup repositories"
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo "Install graphical enviroment"
 sudo dnf install @base-x gnome-shell
 
 echo "Install core applications"
-sudo dnf install gnome-software gnome-terminal nautilus brave-browser gnome-calculator geary gnome-calendar gnome-clocks rhythmbox gedit libreoffice
+sudo dnf install gnome-software gnome-terminal nautilus brave-browser gnome-calculator geary gnome-calendar gnome-clocks rhythmbox gedit libreoffice gnome-system-monitor
 
 echo "Install user specific applications"
-flatpak install ardour audacity discord drawio gnome.extensions lutris lbry obsproject.Studio pitivi signal spotify warpinator gimp xnconvert mpv
+flatpak install org.ardour.Ardour org.audacityteam.Audacity com.discordapp.Discord com.jgraph.drawio.desktop org.gnome.Extensions io.lbry.lbry-app obsproject.Studio org.pitivi.Pitivi org.signal.Signal com.spotify.Client org.x.Warpinator org.gimp.GIMP com.xnview.XnConvert io.mpv.Mpv
 
 echo "Install developer tools"
-flatpak install sublime.merge rider intellij glade textpieces postman dbeaver
-sudo dnf install code dotnet java-latest-openjdk npm rust cargo 
+flatpak install app/com.sublimemerge.App/x86_64/stable com.jetbrains.Rider com.jetbrains.IntelliJ-IDEA-Community app/com.jetbrains.IntelliJ-IDEA-Ultimate/x86_64/stable org.gnome.Glade com.github.liferooter.textpieces com.getpostman.Postman app/io.dbeaver.DBeaverCommunity/x86_64/stable
+sudo dnf install code dotnet java-latest-openjdk npm rust cargo
 
 echo "Enable services"
 sudo systemctl enable gdm.service NetworkManager
 sudo systemctl set-default graphical.target
-touch ~/.xinitrc
-echo "exec /usr/bin/gnome-session" >> ~/.xinitrc
-echo "WaylandEnable=true" >> /etc/gdm/custom.conf # Under [daemon]
