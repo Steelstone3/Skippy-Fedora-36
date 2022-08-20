11f4ce4d377154a6e74e024f841e34ed16826872
Skippy <70329398+Steelstone3@users.noreply.github.com>
2022-08-20 17:54:25 +0100

    Update skippy_gnome_fedora_36.sh

--- skippy_gnome_fedora_36.sh
+++ skippy_gnome_fedora_36.sh
@@ -20,8 +20,15 @@ sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 echo "Install graphical enviroment"
 sudo dnf install @base-x gnome-shell
 
-echo "Install applications"
-sudo dnf install gnome-software gnome-terminal nautilus brave-browser gnome-calculator geary gnome-calendar rhythmbox
+echo "Install core applications"
+sudo dnf install gnome-software gnome-terminal nautilus brave-browser gnome-calculator geary gnome-calendar gnome-clocks rhythmbox gedit libreoffice
+
+echo "Install user specific applications"
+flatpak install ardour audacity discord drawio gnome.extensions lutris lbry obsproject.Studio pitivi signal spotify warpinator gimp xnconvert mpv
+
+echo "Install developer tools"
+flatpak install sublime.merge rider intellij glade textpieces postman dbeaver
+sudo dnf install code dotnet java-latest-openjdk npm rust cargo 
 
 echo "Enable services"
 sudo systemctl enable gdm.service NetworkManager
