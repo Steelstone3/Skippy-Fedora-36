336206b623f8fc8dc69f6dd9fbe4ca72c2d22a92
Skippy <steelstone3@gmail.com>
2022-11-01 00:44:47 +0000

    Update skippy_fedora_36.sh

--- skippy_fedora_36.sh
+++ skippy_fedora_36.sh
@@ -15,9 +15,10 @@ sudo dnf install @base-x @multimedia @standard gnome-shell
 echo "Install core applications"
 sudo dnf install gnome-software gnome-terminal nautilus brave-browser gnome-calculator geary gnome-calendar gnome-clocks rhythmbox gedit libreoffice gnome-system-monitor wget curl
 
-cd ~
-git clone https://github.com/Steelstone3/Linux-Setup.git
-
 echo "Enable services"
 sudo systemctl enable gdm.service NetworkManager
 sudo systemctl set-default graphical.target
+
+echo "Install applications"
+cd ~
+git clone https://github.com/Steelstone3/Linux-Setup.git
