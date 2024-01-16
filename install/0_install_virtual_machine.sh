67f668f95506020248002cae1fa8a32af367228a
Skippy <steelstone3@gmail.com>
2024-01-16 06:30:35 +0000

    Update 0_install_virtual_machine.sh

--- install/0_install_virtual_machine.sh
+++ install/0_install_virtual_machine.sh
@@ -10,3 +10,42 @@ sudo dnf5 install gnome-boxes
 
 # Reclaim space when guest is off
 # virt-sparsify --in-place Fedora-Server-KVM-39-1.5.x86_64.qcow2
+
+# Resize the LVM
+
+# Create a partition
+
+# lsblk
+# fdisk /dev/vda
+# n
+# p
+# partition number: default
+# first sector: default
+# last sector: default
+# t
+# partition 4 (default)
+# L
+# 44
+# w
+
+# Create phyical volume
+# sudo pvcreate /dev/vda4
+
+# Find the volume group name
+# sudo vgdisplay
+# sysvg: volume group name
+
+# Extend the volume group
+# sudo vgextend sysvg /dev/vda4
+# sudo vgdisplay
+# Free PE / Size 23807 / <93.00GiB
+
+# Extend logical volume
+# sudo lvdisplay
+# sudo pvscan
+
+# sudo lvextend -L+5G /dev/sysvg/root
+# sudo lvdisplay: LV Size: 11GB
+# sudo vgdisplay: Free PE / Size: 88GB
+# xfs_growfs /dev/sysvg/root
+# resize2fs /dev/sysvg/root (for ext4)
