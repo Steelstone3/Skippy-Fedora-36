2178ee2f74fc0418ecd9bf9f99ed31aac14278c2
Skippy <steelstone3@gmail.com>
2024-01-16 05:14:54 +0000

    Update 0_install_virtual_machine.sh

--- install/0_install_virtual_machine.sh
+++ install/0_install_virtual_machine.sh
@@ -2,3 +2,11 @@ sudo dnf5 install -y @virtualization
 sudo dnf5 install libvirt -y
 sudo dnf5 install virt-install -y
 sudo dnf5 install gnome-boxes
+
+# Resizing guide
+# qemu-img info Fedora-Server-KVM-39-1.5.x86_64.qcow2
+# qemu-img resize disk.qcow2 100G
+# qemu-img resize disk.qcow2 +20G
+
+# Reclaim space when guest is off
+# virt-sparsify --in-place Fedora-Server-KVM-39-1.5.x86_64.qcow2
