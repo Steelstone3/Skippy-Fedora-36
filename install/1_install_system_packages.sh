c8a0f075b3d2e4ac2868b3e6ba708b67539e8918
Skippy <steelstone3@gmail.com>
2024-01-16 04:22:45 +0000

    update

--- install/1_install_system_packages.sh
+++ install/1_install_system_packages.sh
@@ -21,18 +21,3 @@ git config --global alias.st status
 
 git config --global alias.pu push
 git config --global alias.pl pull
-
-# sudo dnf install gnome-software 
-# sudo dnf install gnome-terminal 
-# sudo dnf install nautilus           
-# sudo dnf install brave-browser
-# sudo dnf install gnome-calculator
-# sudo dnf install geary
-# sudo dnf install gnome-calendar
-# sudo dnf install gnome-clocks
-# sudo dnf install rhythmbox
-# sudo dnf install gedit
-# sudo dnf install libreoffice
-# sudo dnf install gnome-system-monitor
-# sudo dnf install wget
-# sudo dnf install curl
--- install/4_install_user_packages.sh
+++ install/4_install_user_packages.sh
@@ -5,3 +5,17 @@ sudo dnf5 install rhythmbox
 
 flatpak install com.brave.Browser
 flatpak install com.vysp3r.ProtonPlus
+
+# sudo dnf install gnome-software 
+# sudo dnf install nautilus           
+# sudo dnf install brave-browser
+# sudo dnf install gnome-calculator
+# sudo dnf install geary
+# sudo dnf install gnome-calendar
+# sudo dnf install gnome-clocks
+# sudo dnf install rhythmbox
+# sudo dnf install gedit
+# sudo dnf install libreoffice
+# sudo dnf install gnome-system-monitor
+# sudo dnf install wget
+# sudo dnf install curl
