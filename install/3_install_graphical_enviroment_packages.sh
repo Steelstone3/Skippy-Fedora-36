9fac0dd49931bd5d566419370d5ac91750b5d4d9
Skippy <steelstone3@gmail.com>
2024-01-16 07:08:15 +0000

    update

--- install/3_install_graphical_enviroment_packages.sh
+++ install/3_install_graphical_enviroment_packages.sh
@@ -8,8 +8,6 @@ echo "Gnome shell as a test"
 sudo dnf5 install -y gdm
 
 sudo dnf5 install -y gdm
-sudo dnf5 install -y copr
-sudo dnf copr enable th3-s4lm0n/leftwm -y
 sudo dnf5 install -y leftwm
 # sudo dnf5 install -y @gnome-desktop
 # sudo dnf5 install -y gnome-shell
