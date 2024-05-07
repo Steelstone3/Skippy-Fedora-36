a978acb1ae39c19b5b2e54002f25eaf08e0bbeda
Skippy <steelstone3@gmail.com>
2024-05-07 17:28:22 +0100

    Update 3_install_graphical_enviroment_packages.sh

--- install/3_install_graphical_enviroment_packages.sh
+++ install/3_install_graphical_enviroment_packages.sh
@@ -1,9 +1,18 @@
 echo "Install graphical enviroment packages"
 
+sudo dnf5 group list --hidden
+
 sudo dnf5 install -y @minimal
-sudo dnf5 install -y @base-x 
-sudo dnf5 install -y @multimedia 
+sudo dnf5 install -y @base-x
+sudo dnf5 install -y @multimedia
 sudo dnf5 install -y @standard
+sudo dnf5 install -y @hardware-support
+# sudo dnf5 install -y @core
+# sudo dnf5 install -y @networkmanager-submodules
+# sudo dnf5 install -y @container-management
+# sudo dnf5 install -y @anaconda-tools
+# sudo dnf5 install -y @printing
+# sudo dnf5 install -y @fonts
 
 sudo dnf5 install -y lightdm
 sudo dnf5 install -y cinnamon
