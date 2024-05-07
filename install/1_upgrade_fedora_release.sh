c9c8c35a3404183476c656890fd3cdc9a8a0a516
Skippy <steelstone3@gmail.com>
2024-05-07 18:07:36 +0100

    Update 1_upgrade_fedora_release.sh

--- install/1_upgrade_fedora_release.sh
+++ install/1_upgrade_fedora_release.sh
@@ -1,3 +1,3 @@
 sudo dnf5 install dnf-plugin-system-upgrade
-sudo dnf5 system-upgrade download --releasever=40
+sudo dnf5 system-upgrade download --releasever= # Upgrades F39 to F40
 sudo dnf5 offline reboot
