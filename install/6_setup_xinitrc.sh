a374ea8a8e170ac6402cbb48509a730513ff1c01
Skippy <steelstone3@gmail.com>
2024-06-06 02:36:49 +0100

    update

--- install/6_setup_xinitrc.sh
+++ install/6_setup_xinitrc.sh
@@ -1,4 +1,4 @@
-touch ~/.xinitrc
+# touch ~/.xinitrc
 
-echo "exec cosmic-session" > ~/.xinitrc
+# echo "exec cosmic-session" > ~/.xinitrc
 # echo "exec cinnamon-session" > ~/.xinitrc
--- install/7_setup_services.sh
+++ install/7_setup_services.sh
@@ -1,4 +1,7 @@
 echo "Setup services"
 
-sudo systemctl enable lightdm NetworkManager
+# sudo systemctl enable lightdm NetworkManager
+sudo systemctl enable NetworkManager
+sudo systemctl enable cosmic-greeter
+
 sudo systemctl set-default graphical.target
